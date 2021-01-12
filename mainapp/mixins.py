7fad44892634ba768903e0a0b9f26ba3265c6b0d
VanionVina <gamem9120@gmail.com>
2021-01-12 10:11:08 +0500

    Add mixin for left global categorys list

--- mainapp/mixins.py
+++ mainapp/mixins.py
@@ -1,3 +1,12 @@
 from django.contrib.auth.models import ContentType
-from django.views.generic impot View
+from django.views.generic import View
+
+from mainapp.models import GlobalCategory
+
+
+class GetCategorysMixin(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.g_categorys = GlobalCategory.objects.all()
+        return super().dispatch(request, *args, **kwargs)
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,37 +3,36 @@ from django.views.generic import View
 from django.contrib.auth.models import ContentType
 
 
+from .mixins import GetCategorysMixin
 from .models import Category, GlobalCategory
 from .logic import get_products
 
 
-class Index(View):
+class Index(GetCategorysMixin, View):
 
     def get(self, request):
         products = get_products.get_all_products()[:6]
-        global_categorys = GlobalCategory.objects.all()
         context = {
             'products': products,
-            'categorys': global_categorys,
+            'g_categorys': self.g_categorys,
         }
         return render(request, 'index.html', context)
 
 
-class ProductDetail(View):
+class ProductDetail(GetCategorysMixin, View):
 
     def get(self, request, category_slug, product_slug):
         product = get_products.get_product(category_slug, product_slug)
-        global_categorys = GlobalCategory.objects.all()
         spec = get_products.get_product_specif(category_slug, product_slug)
         context = {
             'product': product,
-            'categorys': global_categorys,
+            'g_categorys': self.g_categorys,
             'spec': spec,
         }
         return render(request, 'product_detail.html', context)
 
 
-class GlobalCategoryDetail(View):
+class GlobalCategoryDetail(GetCategorysMixin, View):
 
     def get(self, request, global_category_slug):
         g_category = GlobalCategory.objects.get(slug=global_category_slug)
@@ -42,24 +41,21 @@ class GlobalCategoryDetail(View):
         for category in categorys:
             products_for_category = get_products.get_objects_for_category(category.slug)
             products += products_for_category
-        categorys = GlobalCategory.objects.all()
         context = {
-                'g_category': g_category,
-                'categorys': categorys,
                 'products': products,
+                'g_categorys': self.g_categorys,
         }
         return render(request, 'global_category_detail.html', context)
 
 
-class CategoryDetail(View):
+class CategoryDetail(GetCategorysMixin, View):
 
     def get(self, request, category_slug):
         category = Category.objects.get(slug=category_slug)
         products = get_products.get_objects_for_category(category.slug)
-        categorys = GlobalCategory.objects.all()
         context = {
             'self_category': category,
-            'categorys': categorys,
             'products': products,
+            'g_categorys': self.g_categorys,
         }
         return render(request, 'category_detail.html', context)
